647cd45bff6ff4921d081d032599a65a1130d8a5
BenFaruna <benjaminfaruna@gmail.com>
2022-07-30 14:29:50 +0100

    Completed TDD tutorial

--- test/test_index_e2e.py
+++ test/test_index_e2e.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.service import Service
 class E2ETests(unittest.TestCase):
 
     def setUp(self):
-        service = Service("chromedriver_win32/chromedriver.exe")
+        service = Service("../chromedriver_win32/chromedriver.exe")
         self.driver = webdriver.Chrome(service=service)
         self.driver.get("http://localhost:5000")
 
@@ -19,7 +19,7 @@ class E2ETests(unittest.TestCase):
 
     def test_browser_title_contains_app_name(self):
         self.assertIn("Named Entity", self.driver.title)
-
+ 
     def test_page_heading_is_named_entity_finder(self):
         heading = self._find("heading").text
         self.assertEqual("Named Entity Finder", heading)
@@ -31,3 +31,12 @@ class E2ETests(unittest.TestCase):
     def test_page_has_button_for_submitting_text(self):
         submit_button = self._find("submit-button")
         self.assertIsNotNone(submit_button)
+
+    def test_page_has_ner_table(self):
+        input_element = self._find("input-text")
+        submit_button = self._find("submit-button")
+
+        input_element.send_keys("France and Germany share a border in Europe")
+        submit_button.click()
+        table = self._find("ner-table")
+        self.assertIsNotNone(table)
